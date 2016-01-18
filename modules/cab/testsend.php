b0327f7cf379daf9016863619b0ff3adb16285e5
Andreych1500 <Savitskuy@ukr.net>
2016-01-18 14:42:22 +0200

    Delete testsend.php

--- modules/cab/testsend.php
+++
@@ -1,6 +0,0 @@
-<?php
-// тестова відправка листів
-Mail::$to = 'andreychsavit2@gmail.com';
-Mail::$subject = 'Registration';
-Mail::$text = 'You have registered on the site, in order to confirm this by email please: ';
-Mail::testsend();
