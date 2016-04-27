68e99586d17ea826597fee5f07554f8be9611e21
Andreych1500 <Savitskuy@ukr.net>
2016-04-27 09:45:55 +0300

    new version

--- libs/class_TemplateMail.php
+++ libs/class_TemplateMail.php
@@ -31,16 +31,16 @@ return self::$html = '
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Замовлення товару</title>
 </head>
-<body style="background:url('.$siteDirToFiles.'/skins/default/img/mails/pattern.png) #ffffff; max-width:560px; line-height:1.5; color:#482200; padding:30px 20px; font-size:18px; margin:auto;">
+<body style="background-image:url('.$siteDirToFiles.'/skins/default/img/mails/pattern.png); background-color:#ffffff; line-height:1.5; color:#482200; padding:30px 0; font-size:18px;">
 <style>
 @media screen and (max-width: 480px){ .goods{ width:98% !important; margin-bottom:20px !important; } }
 </style>
-<div role="header" style="text-align: center; border: 1px dashed #43D1E0; background:#ffffff; padding:10px 0; border-bottom: 4px solid #43D1E0;">
+<div role="header" style="text-align: center; border: 1px dashed #43D1E0; background:#ffffff; padding:10px 20px; border-bottom: 4px solid #43D1E0; margin: auto; max-width: 558px;">
 <a href="'.$siteDirToFiles.$lang.'" target="_blank" title="www.childrensdream.com.ua" style="outline:none; display:inline-block;"><img src="'.$siteDirToFiles.'/skins/default/img/mails/cd-static.png" style="width:auto"></a>
 <p style="font-size: 26px; margin:0; font-weight:bold;">'.$mess['ISM'].'</p>
 <div style="clear:both;"></div>
 </div>
-<div role="main" style="border: 1px dashed #43D1E0; border-top: 0; background:#ffffff; font-family: sans-serif; padding: 0 3%; border-bottom: 4px solid #43D1E0; padding-top:10px;">
+<div role="main" style="border: 1px dashed #43D1E0; border-top: 0; background:#ffffff; font-family: sans-serif; padding: 10px 20px 0; border-bottom: 4px solid #43D1E0; margin: auto; max-width: 558px;">
 <p style="margin:0 0 10px;">'.$mess['TEXT_1'].', '.hsc(self::$name_user).'.</p>
 <p style="margin:0 0 10px;">'.$mess['TEXT_2'].'</p>
 <p style="margin:0 0 10px;">'.$mess['TEXT_3'].' №'.hsc(self::$number_order).' '.$mess['TEXT_4'].' '.hsc(self::$order_price).' грн.</p>
@@ -60,8 +60,8 @@ return self::$html = '
 </div>
 <div style="clear:both;"></div>
 </div>
-<div role="footer" style="background:#ffffff; padding: 10px; color: #BFBFBF; text-align: center; border: 1px dashed #43D1E0; border-top: 0;">
-<p style="margin:0 0 10px;">E-mail: <b style="color:#8B8B8B !important;">cdmatrasses@gmail.com</b> | phone: <b style="color:#8B8B8B !important;">+38 (098) 570-43-77</b></p>
+<div role="footer" style="background:#ffffff; padding: 10px 20px; color: #BFBFBF; text-align: center; border: 1px dashed #43D1E0; border-top: 0; margin: auto; max-width: 558px;">
+<p style="margin:0 0 10px;">E-mail: <a href="mailto:cdmatrasses@gmail.com" style="color:#8B8B8B !important; font-weight:bold; text-decoration:none;">cdmatrasses@gmail.com</a> | phone: <b style="color:#8B8B8B !important;">+38 (098) 570-43-77</b></p>
 <div style="font-size:0; margin:0 0 10px;">
 <a href="https://www.facebook.com/tmchildrensdream" target="_blank" style="display:inline-block; margin: 0 5px;"><img src="'.$siteDirToFiles.'/skins/default/img/mails/facebook.png"></a>
 <a href="https://vk.com/tmchildrensdream" target="_blank" style="display:inline-block; margin: 0 5px;"><img src="'.$siteDirToFiles.'/skins/default/img/mails/vk.png"></a>
@@ -69,7 +69,7 @@ return self::$html = '
 <a href="https://www.instagram.com/tm_childrens_dream" target="_blank" style="display:inline-block; margin: 0 5px;"><img src="'.$siteDirToFiles.'/skins/default/img/mails/instagram.png"></a>
 <div style="clear:both;"></div>
 </div>
-<p style="font-size:14px;">'.$mess['TEXT_9'].': <a href="'.$siteDirToFiles.$lang.'" target="_blank" style="color: #43D1E0;">www.childrensdream.com.ua</a></p>
+<p style="font-size:14px;">'.$mess['TEXT_9'].': <a href="'.$siteDirToFiles.$lang.'" target="_blank" style="color: #43D1E0; display: inherit;">www.childrensdream.com.ua</a></p>
 </div>
 </body>
 </html>';
