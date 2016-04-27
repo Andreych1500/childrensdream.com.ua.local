ba838d49e2ed8727020551bbac9b9759b1feabb9
Andreych1500 <Savitskuy@ukr.net>
2016-04-27 09:31:29 +0300

    new version

--- libs/class_TemplateMail.php
+++ libs/class_TemplateMail.php
@@ -7,12 +7,12 @@ class TemplateMail {
     static $goods = array(
         0 => array(
             "name"  => "#NAME_GOODS0#",
-            "link"  => "#LINK_GOODS0#",
+            "link"  => "#LINK_GOODS0",
             "photo" => "/skins/default/img/default/nofoto.png"
         ),
         1 => array(
             "name"  => "#NAME_GOODS1#",
-            "link"  => "#LINK_GOODS1#",
+            "link"  => "#LINK_GOODS1",
             "photo" => "/skins/default/img/default/nofoto.png"
         )
     );
@@ -21,7 +21,6 @@ class TemplateMail {
 
     static function orderHtml($lang, $siteDirToFiles){
         include './libs/lang/'.$lang.'/lang.php';
-
         $lang = (($lang == 'ua')? '/' : '/ru/');
 
 return self::$html = '
@@ -44,7 +43,7 @@ return self::$html = '
 <div role="main" style="border: 1px dashed #43D1E0; border-top: 0; background:#ffffff; font-family: sans-serif; padding: 0 3%; border-bottom: 4px solid #43D1E0; padding-top:10px;">
 <p style="margin-top:0;">'.$mess['TEXT_1'].', '.hsc(self::$name_user).'.</p>
 <p>'.$mess['TEXT_2'].'</p>
-<p>'.$mess['TEXT_3'].' №<b>'.hsc(self::$number_order).'</b> '.$mess['TEXT_4'].' '.hsc(self::$order_price).' грн.</p>
+<p>'.$mess['TEXT_3'].' №'.hsc(self::$number_order).' '.$mess['TEXT_4'].' '.hsc(self::$order_price).' грн.</p>
 <p>'.$mess['TEXT_5'].': #DEVELORY#.</p>
 <p>'.$mess['TEXT_6'].'</p>
 <div style="margin: auto; width: 250px; text-align: center; background: #43D1E0; font-size:20px; margin-bottom: 20px;">
@@ -52,22 +51,22 @@ return self::$html = '
 </div>
 <p style="font-weight:bold; text-align:center;">'.$mess['TEXT_8'].':</p>
 <div class="goods" style="float:left; width:48%; margin:1%; text-align:center;">
-<a target="_blank" style="display:inline-block; width:100%;" href=""><img src="'.hsc(self::$goods[0]['photo']).'" alt="'.hsc(self::$goods[0]['name']).'"></a>
-<a target="_blank" style="width:100%; color:#482200;" href="'.hsc($siteDirToFiles.self::$goods[0]['link']).'">'.hsc(self::$goods[0]['name']).'</a>
+<a href="'.hsc($siteDirToFiles.self::$goods[0]['link']).'" target="_blank" style="display:inline-block; width:100%;"><img src="'.$siteDirToFiles.hsc(self::$goods[0]['photo']).'" alt="'.hsc(self::$goods[0]['name']).'"></a>
+<a href="'.hsc($siteDirToFiles.self::$goods[0]['link']).'" target="_blank" style="width:100%; color:#482200;">'.hsc(self::$goods[0]['name']).'</a>
 </div>
 <div class="goods" style="float:left; width:48%; margin:1%; text-align:center;">
-<a target="_blank" style="display:inline-block; width:100%;" href=""><img src="'.hsc(self::$goods[1]['photo']).'" alt="'.hsc(self::$goods[1]['name']).'"></a>
-<a target="_blank" style="width:100%; color:#482200;" href="'.hsc($siteDirToFiles.self::$goods[1]['link']).'">'.hsc(self::$goods[1]['name']).'</a>
+<a href="'.hsc($siteDirToFiles.self::$goods[1]['link']).'" target="_blank" style="display:inline-block; width:100%;"><img src="'.$siteDirToFiles.hsc(self::$goods[1]['photo']).'" alt="'.hsc(self::$goods[1]['name']).'"></a>
+<a href="'.hsc($siteDirToFiles.self::$goods[1]['link']).'" target="_blank" style="width:100%; color:#482200;">'.hsc(self::$goods[1]['name']).'</a>
 </div>
 <div style="clear:both;"></div>
 </div>
 <div role="footer" style="background:#ffffff; padding: 10px; color: #BFBFBF; text-align: center; border: 1px dashed #43D1E0; border-top: 0;">
-<p style="margin-top:0;">E-mail: <b style="color:#8B8B8B;">cdmatrasses@gmail.com</b> | phone: <b style="color:#8B8B8B;">+38 (098) 570-43-77</b></p>
+<p style="margin-top:0;">E-mail: <b style="color:#8B8B8B !important;">cdmatrasses@gmail.com</b> | phone: <b style="color:#8B8B8B !important;">+38 (098) 570-43-77</b></p>
 <div style="font-size:0;">
-<a href="//www.facebook.com/tmchildrensdream" target="_blank" style="display:inline-block; margin: 0 5px;"><img src="'.$siteDirToFiles.'/skins/default/img/mails/facebook.png"></a>
-<a href="//vk.com/tmchildrensdream" target="_blank" style="display:inline-block; margin: 0 5px;"><img src="'.$siteDirToFiles.'/skins/default/img/mails/vk.png"></a>
-<a href="//plus.google.com/u/0/101299485141835241705" target="_blank" style="display:inline-block; margin: 0 5px;"><img src="'.$siteDirToFiles.'/skins/default/img/mails/google_plus.png"></a>
-<a href="//www.instagram.com/tm_childrens_dream" target="_blank" style="display:inline-block; margin: 0 5px;"><img src="'.$siteDirToFiles.'/skins/default/img/mails/instagram.png"></a>
+<a href="https://www.facebook.com/tmchildrensdream" target="_blank" style="display:inline-block; margin: 0 5px;"><img src="'.$siteDirToFiles.'/skins/default/img/mails/facebook.png"></a>
+<a href="https://vk.com/tmchildrensdream" target="_blank" style="display:inline-block; margin: 0 5px;"><img src="'.$siteDirToFiles.'/skins/default/img/mails/vk.png"></a>
+<a href="https://plus.google.com/u/0/101299485141835241705/posts" target="_blank" style="display:inline-block; margin: 0 5px;"><img src="'.$siteDirToFiles.'/skins/default/img/mails/google_plus.png"></a>
+<a href="https://www.instagram.com/tm_childrens_dream" target="_blank" style="display:inline-block; margin: 0 5px;"><img src="'.$siteDirToFiles.'/skins/default/img/mails/instagram.png"></a>
 <div style="clear:both;"></div>
 </div>
 <p style="font-size:14px;">'.$mess['TEXT_9'].': <a href="'.$siteDirToFiles.$lang.'" target="_blank" style="color: #43D1E0;">www.childrensdream.com.ua</a></p>
