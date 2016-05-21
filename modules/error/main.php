82b3a38fafb42ae759124dbccf3e9baf4e90147d
Andreych1500 <Savitskuy@ukr.net>
2016-05-21 18:03:50 +0300

    new version

--- modules/error/main.php
+++ modules/error/main.php
@@ -3,6 +3,10 @@ Core::$META['title'] = $mess['Error_404'];
 Core::$META['keywords'] = $mess['Error_404'];
 Core::$META['description'] = $mess['Error_404'];
 
+// нова версія файлів
+$arrVersion = q('SELECT `new_resource` FROM `main_core` WHERE `id` = 1')->fetch_assoc();
+$vF = $arrVersion['new_resource'];
+
 if($_SERVER['REDIRECT_STATUS'] == 403){
     header('HTTP/1.0 403 Forbidden');
     $status_error = 403;
