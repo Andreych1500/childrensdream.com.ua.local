d32c22acebb3872b278decdc73f037721b3a2489
Andreych1500 <Savitskuy@ukr.net>
2016-07-31 00:35:46 +0300

    new version

---
+++ modules/cron.php
@@ -0,0 +1,26 @@
+<?php
+// Создаем поток
+$opts = array(
+    'http' => array(
+        'method' => "GET",
+        'header' => "Accept-language: en\r\n"."Cookie: foo=bar\r\n"
+    )
+);
+
+$context = stream_context_create($opts);
+
+// Открываем файл с помощью установленных выше HTTP-заголовков
+$yandex = file_get_contents('https://mc.yandex.ru/metrika/watch.js', false, $context);
+$google = file_get_contents('https://www.google-analytics.com/analytics.js', false, $context);
+
+if($yandex){
+    $fp = fopen($_SERVER['DOCUMENT_ROOT']."/skins/default/get-js/watch.js", "w");
+    fwrite($fp, $yandex);
+    fclose($fp);
+}
+
+if($google){
+    $fp = fopen($_SERVER['DOCUMENT_ROOT']."/skins/default/get-js/analytics.js", "w");
+    fwrite($fp, $google);
+    fclose($fp);
+}
