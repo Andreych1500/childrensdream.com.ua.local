a03ee1ea95c4e366a5126ce0173742d0d6bd08cb
Andreych1500 <Savitskuy@ukr.net>
2016-11-08 16:30:20 +0200

    new version

--- skins/default/js/script.js
+++ skins/default/js/script.js
@@ -260,6 +260,15 @@ $(document).ready(function () {
     // Start
     $(window).resize();
     $(window).scroll();
+
+    // Map
+    var map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+        });
+    }
 });
 
 
