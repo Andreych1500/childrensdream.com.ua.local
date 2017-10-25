5c51f26ebe620761bc2e6fa1b05228b2a0ab5f7a
Andreych1500 <Savitskuy@ukr.net>
2017-10-25 13:51:54 +0300

    new version 20.10.2017

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 error_reporting(0);
 ini_set('display_errors', 0);
 header('Content-Type: text/html; charset=utf-8');
-session_start();
+
+if(!isset($_SESSION)) {
+    session_start();
+}
 
 if(preg_match('#\/catalog\/|\/product\/#ui', $_SERVER['REQUEST_URI'], $matches)) {
     $redirectCatalog = preg_replace('#catalog|product#ui', 'products', $_SERVER['REQUEST_URI']);
--- modules/static/captcha.php
+++ modules/static/captcha.php
@@ -1,5 +1,9 @@
 <?php
-session_start();
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
 $string = "";
 for ($i = 0; $i < 5; $i++) {
     $string .= chr(rand(97, 122));
